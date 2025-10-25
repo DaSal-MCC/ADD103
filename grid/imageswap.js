bfacc4c95cb85cbe412f464948fa8eeb61417617
DaSal-MCC <dsalazar@students.mchenry.edu>
2025-10-25 18:04:11 -0500

    grid images

---
+++ grid/imageswap.js
@@ -0,0 +1,19 @@
+function imgSwap1(){
+    document.getElementById("change").src = "images/crop-after.jpg";
+}
+
+function imgSwap2(){
+    document.getElementById("change").src = "images/exposure-after.jpg";
+}
+
+function imgSwap3(){
+    document.getElementById("change").src = "images/flip-after.jpg";
+}
+
+function imgSwap4(){
+    document.getElementById("change").src = "images/resize-after.jpg";
+}
+
+function imgSwap5(){
+    document.getElementById("change").src = "images/rotate-after.jpg";
+}
